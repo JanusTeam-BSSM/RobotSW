16a74a9cddae49d3e59e3923f3ffcbf25326ff2d
nottalk0922 <parkmuun0922@gmail.com>
2022-09-18 16:16:21 +0100

    Update color.py

--- color.py
+++ color.py
@@ -4,43 +4,44 @@ import cv2
 import numpy as np
 
 def bluecolor(frame):
-  while(1):
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)    # BGR을 HSV로 변환해줌
+  lower = np.array([110,30,30])         
+  upper = np.array([130,255,255])
 
-    lower = np.array([110,30,30])         
-    upper = np.array([130,255,255])
-
-    mask = cv2.inRange(frame, lower, upper)
+  mask = cv2.inRange(frame, lower, upper)
     
-    res = cv2.bitwise_and(frame, frame, mask)
+  res = cv2.bitwise_and(frame, frame, mask)
 
-    return res
+  return res
 
 def greencolor(frame):
-  while(1):
-    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-    
-    lower = np.array([135,65,65])
-    upper = np.array([58,166,85])
+       
+  lower = np.array([135,65,65])
+  upper = np.array([58,166,85])
   
-    mask = cv2.inRange(frame, lower, upper)
+  mask = cv2.inRange(frame, lower, upper)
 
     
-    res = cv2.bitwise_and(frame, frame, mask)
+  res = cv2.bitwise_and(frame, frame, mask)
 
-    return res
+  return res
 
 def redcolor(frame):
-  while(1):
-    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-    
-    # upper boundary RED color range values; Hue (160 - 180)
-    lower = np.array([160,50,50])
-    upper = np.array([180,255,255])
+
+  lower = np.array([160,50,50])
+  upper = np.array([180,255,255])
   
+  mask = cv2.inRange(frame, lower, upper)
+
+  res = cv2.bitwise_and(frame, frame, mask)
+
+  return res
+
+def section(frame):
+    lower = np.array([])
+    upper = np.array([])
+
     mask = cv2.inRange(frame, lower, upper)
 
-    
     res = cv2.bitwise_and(frame, frame, mask)
 
     return res
