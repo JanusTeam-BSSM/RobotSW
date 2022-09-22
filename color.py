6fdccddd81d07d6e7b6118c714f5ca1209000053
nottalk0922 <106054657+nottalk0922@users.noreply.github.com>
2022-09-22 10:05:26 +0900

    Fix color.py

--- color.py
+++ color.py
@@ -1,47 +1,42 @@
-# b,g,r hsv 값 테스트 해보면서 맞춰야 함
-
 import cv2
 import numpy as np
 
 def bluecolor(frame):
-  lower = np.array([110,30,30])         
-  upper = np.array([130,255,255])
 
-  mask = cv2.inRange(frame, lower, upper)
-    
-  res = cv2.bitwise_and(frame, frame, mask)
-
+  lower = np.array([])          # 파랑색 범위
+  upper = np.array([])
+  
+  img_mask = cv2.inRange(frame, lower, upper)
+  res = cv2.bitwise_and(frame, frame, mask=img_mask)
+  
   return res
 
 def greencolor(frame):
-       
-  lower = np.array([135,65,65])
-  upper = np.array([58,166,85])
+      
+  lower = np.array([])
+  upper = np.array([])
   
-  mask = cv2.inRange(frame, lower, upper)
-
-    
-  res = cv2.bitwise_and(frame, frame, mask)
+  img_mask = cv2.inRange(frame, lower, upper)
+  res = cv2.bitwise_and(frame, frame, mask=img_mask)
 
   return res
 
 def redcolor(frame):
-
-  lower = np.array([160,50,50])
-  upper = np.array([180,255,255])
   
-  mask = cv2.inRange(frame, lower, upper)
-
-  res = cv2.bitwise_and(frame, frame, mask)
+  lower = np.array([0, 0, 100])
+  upper = np.array([100,100,255])
+  
+  img_mask = cv2.inRange(frame, lower, upper)  
+  res = cv2.bitwise_and(frame, frame, mask=img_mask)
 
   return res
+  
+def sectioncolor(frame):
+    
+  lower = np.array([])
+  upper = np.array([])
+  
+  img_mask = cv2.inRange(frame, lower, upper)
+  res = cv2.bitwise_and(frame, frame, mask=img_mask)
 
-def section(frame):
-    lower = np.array([])
-    upper = np.array([])
-
-    mask = cv2.inRange(frame, lower, upper)
-
-    res = cv2.bitwise_and(frame, frame, mask)
-
-    return res
+  return res
