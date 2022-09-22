3aae8b93ec8c4979a7d40549acd2fce8f6362b9e
nottalk0922 <pmu42094@naver.com>
2022-09-22 12:40:04 +0100

    Update Triangle_Detection.py

--- Triangle_Detection.py
+++ Triangle_Detection.py
@@ -25,9 +25,9 @@ def Direction_Check(approx):
     return res
 
 
-def Triangle_Detection(frame):
+def Triangle_Detection(img):
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     _, img_bin = cv2.threshold(
         gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)  # 이진화
@@ -35,22 +35,21 @@ def Triangle_Detection(frame):
     # 외곽선 검출, EXTERNAL로 바깥 외곽선만 도출
     contours, _ = cv2.findContours(
         img_bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
+    
     # 외곽선 좌표 받아오기
-
+    cv2.imshow('img',img)
     for pts in contours:
 
         if cv2.contourArea(pts) < 400:  # contourArea() 면적 반환 / 노이즈 제거, 너무 작으면 무시
             continue
         
-        img = cv2.drawContours(frame, pts, -1, (0, 255, 0), 4)
+        
         # 근사화
         approx = cv2.approxPolyDP(pts, cv2.arcLength(pts, True)*0.02, True)
         print(approx)    
         
         # 근사화 결과 점 갯수
         vtc = len(approx)
-        cv2.imshow('img',img)
         if vtc == 3:
             res = Direction_Check(approx) 
             return res
